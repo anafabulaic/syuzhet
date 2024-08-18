46c9f1db0daca06ea111af2b5cdf4d134ddf1cc8
anafabulaic <114112000+anafabulaic@users.noreply.github.com>
2024-08-18 02:17:49 -0400

    mobile support attempt 2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1723960495|279329139';
+const CACHE_VERSION = '1723961856|26875475';
 /** @type {string} */
 const CACHE_PREFIX = 'DANMAKUPROJ-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
